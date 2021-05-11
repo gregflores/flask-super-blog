27fb59945feea421cdfbfb8e527fcb1c0c6410af
Greg Flores <floresgreg@hotmail.com>
2021-05-10 20:27:25 -0500

    Pagination and password resets

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SOME REALLY LONG RANDOM STRING'
@@ -11,5 +16,12 @@ bcrypt = Bcrypt(app)
 login_manager=LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = os.environ.get('SMTP_SET')
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+print(os.environ.get('EMAIL_USER'))
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskblog import routes
